4b2be47e40a1a727c90dfab05b5fc465ad3dfe8d
doonnn <slink00.bd2@gmail.com>
2017-10-03 18:10:17 +0700

    landing page สิทธิประโยชน์ สำหรับพนักงานบริษัท 5

--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -1,49 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router';
-import { NavLink } from 'react-router-dom';
-import RaisedButton from 'material-ui/RaisedButton';
-import FontIcon from 'material-ui/FontIcon';
-import KKProductInfoImg from '../../assets/kk-product-info.jpg';
+import { Link } from 'react-router-dom';
+
+import Logo from '../../assets/artboard-1-copy-2@3x.png';
 import KKProductInfoPdf from '../../assets/kk-ploan-factsheet.pdf';
 
-const styles = {
-  button: {
-    margin: 12,
-  },
-};
-
-class ProductInfo extends Component {
-  render() {
-    return (
-      <div className="row" style={{ textAlign: 'center' }}>
-        <div className="col-12">
-          <img alt="ProductInfo" src={KKProductInfoImg} style={{ width: '100%' }} />
-        </div>
-        <div className="col-12">
-          <a href={KKProductInfoPdf} download="ploan-factsheet.pdf">
-            <RaisedButton
-              label="ดาวน์โหลด Factsheet สินเชื่อส่วนบุคคล"
-              labelPosition="before"
-              primary
-              style={styles.button}
-              icon={<FontIcon className="muidocs-icon-custom-github" />}
-              onClick={this.handleNext}
-            />
-          </a>
-          <NavLink to="/borrow-request">
-            <RaisedButton
-              label="สนใจส่งคำขอสินเชื่อส่วนบุคคล"
-              labelPosition="before"
-              primary
-              style={styles.button}
-              icon={<FontIcon className="muidocs-icon-custom-github" />}
-              onClick={this.handleNext}
-            />
-          </NavLink>
+const ProductInfo = () => (
+  <div className="landing--container">
+    <div className="landing--container--box">
+      <h1 className="landing--title">สิทธิประโยชน์<br />สำหรับพนักงานบริษัท</h1>
+      <div className="landing--text landing--text--lg">วันนี้คุณสามารถขอสินเชื่อออนไลน์ได้<span className="mbr">ผ่านระบบมันนี่เทเบิล</span></div>
+      <div className="landing--text landing--text--primary landing--text--desktop">
+        <ul className="landing--list">
+          <li>ใช้ง่าย</li>
+          <li>สะดวก</li>
+          <li>ปลอดภัย</li>
+        </ul>
+      </div>
+      <div className="landing--group--g1">
+        <div className="landing--text">คุณได้รับสิทธิใช้บริการเนื่องจากคุณอยู่ในบริษัท<span className="mbr">ที่เป็นพันธมิตรกับเรา</span></div>
+        <Link to="/borrow-request" className="landing--btn">ส่งคำขอสินเชื่อผ่านระบบ</Link>
+      </div>
+      <div className="landing--text landing--text--primary landing--text--mobile">
+        <ul className="landing--list">
+          <li>ใช้ง่าย</li>
+          <li>สะดวก</li>
+          <li>ปลอดภัย</li>
+        </ul>
+      </div>
+      <div className="landing--logo-poster">
+        <div className="landing--logo-poster--bg">
+          <div className="landing--logo-poster--info">
+            <img className="landing--logo-poster--info--logo m-inline" src={Logo} alt="Logo" />
+            <div className="landing--text landing--text--lg m-inline">มันนี่เทเบิลคืออะไร</div>
+            <div className="landing--text landing--logo-poster--desc">มันนี่เทเบิลคือระบบแพลตฟอร์มที่ให้บริการในการจัดการสินเชื่อระหว่างผู้ให้สินเชื่อที่น่าเชื่อถือกับผู้รับสินเชื่อ </div>
+            <Link to={KKProductInfoPdf} target="_blank" className="landing--link landing--text">อ่านข้อมูลผลิตภัณฑ์</Link>
+          </div>
         </div>
       </div>
-    );
-  }
-}
+      <div className="landing--text--bottom">
+        <div className="landing--text landing--text--primary">* การพิจารณาและอนุมัติสินเชื่อ หรืออัตราดอกเบี้ยของสินเชื่อแต่ละประเภทเป็นไปตามเงื่อนไขที่ผู้ให้สินเชื่อที่น่าเชื่อถือเป็นผู้กำหนด</div>
+      </div>
+      <div className="landing--text landing--text--copyright">
+        © 2017 Money.work | All rights reserved.
+      </div>
+    </div>
+  </div>
+);
 
 export default withRouter(ProductInfo);
