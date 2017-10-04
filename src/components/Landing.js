5730e7b1c740b6f294a639edbb9a5f93036b9c68
doonnn <slink00.bd2@gmail.com>
2017-10-04 14:57:47 +0700

    landing index

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
+import HeaderLogo from '../../assets/artboard-1-copy-4@3x.png';
+import SystemPreview from '../../assets/group@2x.png';
 
 const Landing = ({ history }) =>
-  <div>
-    <br /><br /><br />
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      <span>Landing Component</span>
+  <div className="landing--home">
+    <div className="landing--header--logo">
+      <img src={HeaderLogo} alt="Lading page logo" />
     </div>
-    <br />
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      <input
-        type="button"
-        value="Go to Login"
-        onClick={() => history.push('/login')}
-      />
+    <div className="row">
+      <div className="col-md-5">
+        <div className="landing--group--left">
+          <div className="landing--group--box">
+            <h1 className="landing--title">ยินดีต้อนรับสู่<br />มันนี่เทเบิล</h1>
+            <div className="landing--text _clear">
+              <ul className="landing--list">
+                <li>สะดวก</li>
+                <li>ง่าย</li>
+                <li>ได้ชัวร์</li>
+              </ul>
+            </div>
+            <input
+              type="button"
+              value="เข้าสู่ระบบ"
+              className="landing--btn landing--btn--main"
+              onClick={() => history.push('/login')}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-md-7" style={{ zIndex: '1' }}>
+        <div className="landing--index">
+          <div className="landing--index--bg">
+            <img src={SystemPreview} alt="System preview" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="landing--text landing--text--copyright">
+        © 2017 Money.work | All rights reserved.
     </div>
   </div >;
 
