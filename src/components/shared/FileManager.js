a8d7b7f684a9e7cbee8b68bb2b15e3def6945470
j2sdk_ball2 <codecrazii@gmail.com>
2017-10-10 15:52:35 +0700

    reload documents on open FileManager

--- src/components/shared/FileManager.js
+++ src/components/shared/FileManager.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../../styles/loader.scss';
 
 class FileManager extends Component {
 
@@ -14,6 +15,7 @@ class FileManager extends Component {
     if (id && loadDocuments) {
       loadDocuments(id, documents => {
         const urls = documents.map(({ url }) => url);
+
         $('#file1').fileinput({
           theme: 'explorer-fa',
           uploadUrl: '#',
@@ -38,28 +40,36 @@ class FileManager extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    this.setState({ dont: false });
+
     const { id, loadDocuments } = nextProps;
 
     if (id && loadDocuments) {
       loadDocuments(id, documents => {
         const urls = documents.map(({ url }) => url);
-        $('#file1').fileinput({
-          theme: 'explorer-fa',
-          uploadUrl: '#',
-          overwriteInitial: false,
-          initialPreviewAsData: true,
-          initialPreview: urls || [],
-          initialPreviewConfig: [
-          ],
-          language: 'en',
-          showCaption: true,
-          showPreview: true,
-          showRemove: true,
-          showUpload: false,
-          showCancel: true,
-          showUploadedThumbs: true,
-        });
-        // console.log('componentDidMount.urls: ', urls);
+
+        $('#file1').fileinput('destroy');
+
+        setTimeout(() => {
+          $('#file1').fileinput({
+            theme: 'explorer-fa',
+            uploadUrl: '#',
+            overwriteInitial: false,
+            initialPreviewAsData: true,
+            initialPreview: urls || [],
+            initialPreviewConfig: [
+            ],
+            language: 'en',
+            showCaption: true,
+            showPreview: true,
+            showRemove: true,
+            showUpload: false,
+            showCancel: true,
+            showUploadedThumbs: true,
+          });
+          this.setState({ dont: true });
+        }
+          , 100);
       });
     }
   }
@@ -70,8 +80,18 @@ class FileManager extends Component {
       return <div className="loader" />;
     }
 
+    const { id } = this.props;
     return (
       <form encType="multipart/form-data">
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <span>Lead ID: {id}</span>
+        </div>
+        <br />
         <div className="file-loading">
           <input
             id="file1"
